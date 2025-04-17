d887322f6efc27ac57a8842536a8461c9762ac89
Harshita Nahata <khushinahata04@gmail.com>
2025-04-17 11:01:33 +0530

    ->integrate with instance of io ->frontend for Join component(name & room) ->CSS for chat application

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import Join from './components/Join';
-import Chat from './components/Chat';
+import Join from './components/Join/Join';
+import Chat from './components/Chat/Chat';
 
 const App = () => (
-    <Router>
+    <Router>    
         <Routes>
             <Route path="/" element={<Join />} />
             <Route path="/chat" element={<Chat />} />
