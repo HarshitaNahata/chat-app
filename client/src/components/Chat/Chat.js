c8028fb6891f9510d9914447f1ddf3b10c392118
Harshita Nahata <khushinahata04@gmail.com>
2025-04-18 15:39:12 +0530

    basic chat functionality

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,8 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useLocation } from 'react-router-dom'; // 👈 useLocation hook
+import queryString from 'query-string';
+import io from 'socket.io-client';
+
+let socket;
+
 const Chat = () => {
+    const [name, setName] = useState('');
+    const [room, setRoom] = useState('');
+
+    const location = useLocation(); // ✅ get location safely
+    const ENDPOINT = 'http://localhost:5000';
+
+    useEffect(() => {
+        const { name, room } = queryString.parse(location.search);
+        socket = io(ENDPOINT);
+
+        setName(name);
+        setRoom(room);
+
+        socket.emit('join', { name, room });
+
+        return () => {
+            socket.emit('disconnect');
+            socket.off();
+        };
+    }, [ENDPOINT, location.search]);
+
     return (
-        <h1>Chat</h1>
-    )
-}
+        <div>
+            <h1>Chat</h1>
+        </div>
+    );
+};
 
 export default Chat;
