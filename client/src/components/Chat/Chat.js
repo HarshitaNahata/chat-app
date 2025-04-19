154596e5b7217ba1d370484f9b48665893b8cc9a
Harshita Nahata <khushinahata04@gmail.com>
2025-04-19 19:11:26 +0530

    ->creation and styling of text field for chat box

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -3,11 +3,18 @@ import { useLocation } from 'react-router-dom'; // 👈 useLocation hook
 import queryString from 'query-string';
 import io from 'socket.io-client';
 
+import './Chat.css';
+import InfoBar from '../InfoBar/InfoBar';
+import Input from '../Input/Input';
+
 let socket;
+
 
 const Chat = () => {
     const [name, setName] = useState('');
     const [room, setRoom] = useState('');
+    const [message, setMessage] = useState('');
+    const [messages, setMessages] = useState([]);
 
     const location = useLocation(); // ✅ get location safely
     const ENDPOINT = 'http://localhost:5000';
@@ -27,9 +34,34 @@ const Chat = () => {
         };
     }, [ENDPOINT, location.search]);
 
+    useEffect(() => {
+        socket.on('message', (message) => {
+            //adds every new message sent by admin or anyone to the messages array
+            setMessages([...messages, message]);
+        })
+    }, [messages]);
+
+    const sendMessage = (event) => {
+        //preventing the default behavior  as you cick anywhwere the whole page is refreshed
+        event.preventDefault();
+        if (message) {
+            socket.emit('sendMessage', message, () => setMessage(''))
+        }
+    }
+
+    console.log(message, messages);
     return (
-        <div>
-            <h1>Chat</h1>
+        //input for sending messages
+        <div className="outerConatiner">
+            <div className="container">
+                <InfoBar room={room} />
+                <Input message={message} setMessage={setMessage} sendMessage={sendMessage} />
+                {/* <input
+                    value={message}
+                    onChange={(event) => setMessage(event.target.value)}
+                    onKeyPress={event => event.key === 'Enter' ? sendMessage(event) : null}
+                /> */}
+            </div>
         </div>
     );
 };
